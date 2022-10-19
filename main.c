2bf52ef7096066e451da9a3b47e5ead386cff3d7
luhmonts <84717981+Lualuh@users.noreply.github.com>
2022-10-19 22:13:53 +0000

    Versão Mais Avançada do Jogo.

--- main.c
+++ main.c
@@ -11,9 +11,11 @@ int main() {
     printf ("0. Pedra\n");
     printf ("1. Papel\n");
     printf ("2. Tesoura\n");
-    printf ("3. Sair\n");
+    printf ("3. Largarto");
+    printf ("4. Sponk");
+    printf ("5. Sair\n");
     scanf ("%d", &jogador);
-    if (jogador < 0 || jogador >=3)
+    if (jogador < 0 || jogador >=5)
     {
         exit(0);
     }
@@ -24,8 +26,12 @@ int main() {
         case 0: printf ("COMP -> Pedra\n"); break;
         case 1: printf ("COMP -> Papel\n"); break;
         case 2: printf ("COMP -> Tesoura\n"); break;
+		case 3: printf ("COMP -> Largarto\n"); break;   
+		case 4: printf ("COMP -> Sponk\n"); break;
     }
-    if ((jogador == 0 && comp==2) || (jogador == 1 && comp == 0) || (jogador == 2 && comp == 1)) 
+    if ((jogador == 0 && comp==2) || (jogador == 1 && comp == 0) || (jogador == 2 && comp == 1) 
+    (jogador == 0 && comp==3) || (jogador==3 && comp==4) || (jogador==4 && comp==2) || (jogador==2 && comp==3) || (jogador==3 && comp==1) || 
+    (jogador==1 && comp==4) || (jogador==4 && comp==0))
         {
             printf("Voce venceu!\n");
             pontos_jogador++;
@@ -35,6 +41,17 @@ int main() {
    } else {
            printf ("Comp venceu!\n");
             pontos_comp++;
+
+        }
+       
+	do {
+		printf ("Deseja jogar novamente? 1.sim 2.não");
+		scanf ("%d", &d);
+		if (d!=1&&d!=2) 
+			printf ("Inválido");
+		}while (d!=1&&d!=2);
+	}while (d==1);
+}
 
         }
        
